949e6baadd9103838ad983e84b7206a751ed5784
Stavan <sthak004@ucr.edu>
2016-10-29 18:27:22 -0700

    added: basic menu

--- src/8_puzzle_solver.cpp
+++ src/8_puzzle_solver.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
+
+void print_menu() {
+	char userInput;
+	char newline = '\n';
+	cout << "Welcome to the 8-puzzle solver!" << endl;
+	cout << "Designed and coded by Stavan Thaker" << endl;
+	cout << "Please enter to continue or 'q' to quit.";
+	cout << endl << ">> ";
+	userInput = cin.get();
+	if (userInput == 'q') { exit(0); }
+	else if (userInput == '\n') {
+		cout << endl;
+		cout << "Please choose from the following options." << endl;
+		cout << "1. Choose default puzzle." << endl << "2. Enter your own puzzle" << endl;
+		//User input to vector table function 
+	}
+}
+
 int main() {
-	cout << "Hello World!" << endl;
+	char userInput;
+	/*while(userInput != 'q') {
+		if(userInput != 'q' && userInput == '\n') {
+			cout << "Enter the first row.";
+			cout << "Please enter each number followed by a newline" << endl;
+			int a, b, c;
+			cin >> a;
+			cin >> b;
+			cin >> c;
+			cout << "Your choices are: " << a << b << c << endl;
+			break;
+		}
+	}*/
+	while(1) {
+		print_menu();
+	}
 	return 0;
 }
