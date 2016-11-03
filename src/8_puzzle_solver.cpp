54926a6d6cb0e05889fdc6cf65e0865fc1d07d7a
Stavan <sthak004@ucr.edu>
2016-11-02 22:35:04 -0700

    added operators to problem space

--- src/8_puzzle_solver.cpp
+++ src/8_puzzle_solver.cpp
@@ -7,6 +7,9 @@ using namespace std;
 
 #define PUZZLE_WIDTH 3 /* DIMENSION OF PUZZLE (ex. 3 x 3) */
 
+
+vector<int> GOAL_STATE = {1, 2, 3, 4, 5, 6, 7, 8, 0}; /* GOAL VECTOR */
+
 void print_v(vector<int> v) {
 	cout << endl;
 	for(int i = 0; i < v.size(); i++) {
@@ -20,7 +23,6 @@ void print_vector(vector<int> &arg) {
 	string spaceBetweenEntries = "  ";
 
 	cout << endl << endl;
-	cout << "SIZE OF PUZZLE: " << arg.size() << endl << endl;
 	cout << "\tPuzzle" << endl;
 	cout << "-----------------------" << endl << "\t";
 	for(int i = 0; i < arg.size(); i++) {
@@ -31,12 +33,153 @@ void print_vector(vector<int> &arg) {
 }
 
 
-void default_puzzle() {
-	cout << "You chose the deafult puzzle" << endl;
+
+/* ----------------------------------------------------------------------------- */
+
+/* bp = position of blank*/
+bool isLeftWall(int bp) {	
+	if(bp == 0 || bp == 3 || bp == 6) {
+		return true;
+	}
+	else { return false; }
 }
 
 
-void custom_puzzle() {
+bool isRightWall(int bp) {
+	if(bp == 2 || bp == 5 || bp == 8) {
+		return true;
+	}
+	else {return false; }
+}
+
+
+
+bool isTopWall(int bp) {
+	if(bp == 0 || bp == 1 || bp == 2) {
+		return true;
+	}
+	else {return false; }
+}
+
+
+
+bool isBottomWall(int bp) {
+	if(bp == 6 || bp == 7 || bp == 8) {
+		return true;
+	}
+	else {return false; }
+}
+
+
+/* ----------------------------------------------------------------------------- */
+
+/* Finds location of blank */
+int find_blank(vector<int> &state) {
+	for(int i = 0; i < state.size(); i++){
+		if(state.at(i) == 0) {
+			return i; 
+		}
+	}
+}
+
+/* move blank space LEFT  */
+void move_left(vector<int> &state) {
+	int blank = find_blank(state);
+
+	/* IF blank space is NOT on left wall*/
+	if(!isLeftWall(blank)) {
+		cout << "Moving left..." << endl;
+		swap(state.at(blank), state.at(blank-1));
+	}
+	else {
+		cout << endl << "Left wall, cannot move it." << endl;
+	}
+	print_vector(state);
+}
+
+
+/* move blank space RIGHT */
+void move_right(vector<int> &state) {
+	int blank = find_blank(state);
+
+	/* IF blank space is NOT on left wall*/
+	if(!isRightWall(blank)) {
+		cout << "Moving right..." << endl;
+		swap(state.at(blank), state.at(blank+1));
+	}
+	else {
+		cout << endl << "Right wall, cannot move it." << endl;
+	}
+	print_vector(state);
+}
+
+
+/* move blank space UP */
+void move_up(vector<int> &state) {
+	int blank = find_blank(state);
+
+	/* IF blank space is NOT on left wall*/
+	if(!isTopWall(blank)) {
+		cout << "Moving up..." << endl;
+		int index_above = (PUZZLE_WIDTH * ((blank/PUZZLE_WIDTH) - 1 ) + (blank % PUZZLE_WIDTH));
+		swap(state.at(blank), state.at(index_above));
+	}
+	else {
+		cout << endl << "Top wall, cannot move it." << endl;
+	}
+	print_vector(state);
+}
+
+
+/* move blank space DOWN */
+void move_down(vector<int> &state) {
+	int blank = find_blank(state);
+
+	/* IF blank space is NOT on left wall*/
+	if(!isBottomWall(blank)) {
+		cout << "Moving down..." << endl;
+		int index_below = (PUZZLE_WIDTH * ((blank/PUZZLE_WIDTH) + 1 ) + (blank % PUZZLE_WIDTH));
+		swap(state.at(blank), state.at(index_below));
+	}
+	else {
+		cout << endl << "Bottom wall, cannot move it." << endl;
+	}
+	print_vector(state);
+}
+
+
+/* ----------------------------------------------------------------------------- */
+
+void uniform_cost(vector<int> &state) {
+	move_up(state);
+	move_up(state);
+	move_up(state);
+}
+
+
+void misplaced_tile() {
+	cout << endl << "you chose misplaced_tile" << endl;
+}
+
+
+void manhatatan_distance() {
+	cout << endl << "you chose manhatatan_distance" << endl;
+}
+
+/* ----------------------------------------------------------------------------- */
+
+
+
+vector<int> default_puzzle() {
+	/* dp = the default puzzle*/
+	vector<int> dp = {1, 2, 3, 4, 5, 6, 7, 8, 0};
+	cout << "DEFAULT PUZZLE: " << endl;
+	print_vector(dp);
+	return dp;
+}
+
+
+vector<int> custom_puzzle() {
 	cout << "Enter your puzzle. Use '0' as the blank space" << endl;
 	int entry;
 	vector<int> table;
@@ -76,8 +219,21 @@ void custom_puzzle() {
 	}
 
 	print_vector(table);
+	return table;
 }
 
+
+int choose_search() {
+	int algorithm_choice;
+	cout << endl << endl;
+	cout << "Enter your choice of algorithm" << endl;
+	cout << "1. Uniform Cost Search" << endl;
+	cout << "2. A* with the Misplaced Tile Heuristic" << endl;
+	cout << "3. A* with the Manhattan Distance Heuristic" << endl;
+	cout << endl << ">> ";
+	cin >> algorithm_choice;
+	return algorithm_choice;
+}
 
 void print_menu() {
 	char userInput;
@@ -90,6 +246,9 @@ void print_menu() {
 	if (userInput == 'q') { exit(0); }
 	else if (userInput == '\n') {
 		cout << endl;
+
+		vector<int> starting_state;
+
 		int puzzleChoice;
 		cout << "Please choose from the following options." << endl;
 		cout << "1. Choose default puzzle." << endl
@@ -100,16 +259,36 @@ void print_menu() {
 
 		if(puzzleChoice == 1) {
 			cout << endl;
-			default_puzzle();
+			starting_state = default_puzzle();
+			cout << "SIZE OF PUZZLE: " << starting_state.size() << endl << endl;
 		}
 		else if (puzzleChoice == 2) {
 			cout << endl;
-			custom_puzzle();
+			starting_state = custom_puzzle();
+			cout << "SIZE OF PUZZLE: " << starting_state.size() << endl << endl;
 		}
 		else {
 			cout << "Please choose option '1' or '2'" << endl;
 			return;
 		}
+
+		int search_choice = choose_search();
+		switch(search_choice) {
+			case 1:
+				uniform_cost(starting_state);
+				break;
+			case 2:
+				misplaced_tile();
+				break;
+			case 3:
+				manhatatan_distance();
+				break;
+			default:
+				cout << "\t\t***** ERROR *****" << endl;
+				cout << "\tBad input, quitting..." << endl;
+				break;
+		}
+
 	}
 }
 
